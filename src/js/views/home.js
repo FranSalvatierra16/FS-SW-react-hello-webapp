e4b460dda56eaf5c724c7eaa8e0fe5248235db1c
Francisco Salvatierra <103911827+FranSalvatierra16@users.noreply.github.com>
2023-11-06 21:28:56 +0000

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,121 +1,167 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/index.css";
-import DetailPage from "./detailPage.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Carousel, Card, Row, Col, Button, Container, Navbar, Nav, Modal } from "react-bootstrap";
+import "../../styles/home.css";
+import { FaTimes } from "react-icons/fa";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-	const [selectedItem, setSelectedItem] = useState(null);
-	const [favorites, setFavorites] = useState([]);
+    const { store, actions } = useContext(Context);
+    const [favorites, setFavorites] = useState([]);
+    const [showFavoritesModal, setShowFavoritesModal] = useState(false);
+    const [selectedFavorite, setSelectedFavorite] = useState(null);
+    const itemsPerRow = 4;
 
-	useEffect(() => {
-		actions.cargarPlanetas();
-		actions.cargarPersonajes();
-		actions.cargarFilms();
-		actions.cargarVehicles();
-	}, []);
+    useEffect(() => {
+        actions.cargarPlanetas();
+        actions.cargarPersonajes();
+        actions.cargarVehicles();
+    }, []);
 
-	function inputKeyDown(e) {
-		if (e.key === "Enter") {
-			actions.agregarTodos(e.target.value);
-		}
-	}
-	function handleItemClick(item) {
+    function addToFavorites(item) {
+        setFavorites([...favorites, item]);
+    }
 
 
-		setSelectedItem(item);
-	}
-	function handleItemClick1(item) {
-
-		item.img = `https://starwars-visualguide.com/assets/img/films/${item.uid}.jpg`
-
-		setSelectedItem(item);
-	}
-
-	function handleItemClick2(item) {
-
-		item.img = `https://starwars-visualguide.com/assets/img/vehicles/${item.uid}.jpg`
-
-		setSelectedItem(item);
-	}
-	function handleCloseDetail() {
-		setSelectedItem(null);
-	}
-	function addToFavorites(item) {
-		setFavorites([...favorites, item]);
-	}
-
-	return (
-		<div className="home">
-			<header className="header">
-				<h1>Star Wars </h1>
-			</header>
-			<div className="content">
-				<section className="section">
-					<h2>Planetas</h2>
-					<div className="card-grid">
-						{store.planets.map((planet) => (
-							<div key={planet.id} className="card" onClick={() => handleItemClick(planet)}>
-								<img src={planet.img} alt={planet.name} />
-								<div className="card-title">{planet.name}</div>
-								<button className="favorite-button" onClick={() => addToFavorites(planet)}>
-									Añadir a favoritos
-								</button>
-							</div>
-						))}
-					</div>
-				</section>
-
-			</div>
-			<section className="section">
-				<h2>Personajes</h2>
-				<div className="card-grid">
-					{store.characters.map((character) => (
-						<div key={character.id} className="card" onClick={() => handleItemClick(character)}>
-							<img src={character.img} alt={character.name} />
-							<div className="card-title">{character.name}</div>
-							<button className="favorite-button" onClick={() => addToFavorites(character)}>
-								Añadir a favoritos
-							</button>
-						</div>
-					))}
-				</div>
-			</section>
+    function removeFromFavorites(favItem) {
+        const newFavorites = [...favorites];
+        const index = newFavorites.findIndex((item) => item.id === favItem.id);
+        if (index > -1) {
+            newFavorites.splice(index, 1);
+            setFavorites(newFavorites);
+        }
+    }
 
 
-			<h2>Películas</h2>
-			<section className="section">
 
-				<div className="card-grid">
-					{store.films.map((film) => (
-						<div key={film.id} className="card" onClick={() => handleItemClick1(film)}>
-							<img src={`https://starwars-visualguide.com/assets/img/films/${film.uid}.jpg`} alt={film.name} />
-							<div className="card-title">{film.name}</div>
+    function createCarouselItems(items, itemsPerRow) {
+        const carouselItems = [];
+        let currentRow = [];
 
-						</div>
-					))}
-				</div>
-			</section>
-			<h2>Vehiculos</h2>
-			<section className="section">
+        items.forEach((item, index) => {
+            currentRow.push(
+                <Col key={item.id} className="custom-card-col">
+                    <Card className="custom-card">
+                        <Card.Img
+                            className="custom-card-img"
+                            variant="top"
+                            src={item.img}
+                            alt={item.name}
+                        />
+                        <Card.Body>
+                            <Card.Title>{item.name}</Card.Title>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <Button
+                                    className="favorite-button btn-danger"
+                                    onClick={() => addToFavorites(item)}
+                                >
+                                    Añadir a favoritos
+                                </Button>
+                                <Button
+                                    className="details-button btn-danger"
+                                    onClick={() => openDetails(item)}
+                                >
+                                    Ver Más
+                                </Button>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            );
 
-				<div className="card-grid">
-					{store.vehicles.map((vehicle) => (
-						<div key={vehicle.id} className="card" >
-							<img src={`https://starwars-visualguide.com/assets/img/vehicles/${vehicle.uid}.jpg`} onClick={() => handleItemClick2(vehicle)} alt={vehicle.name} />
+            if (currentRow.length === itemsPerRow || index === items.length - 1) {
+                carouselItems.push(
+                    <Carousel.Item key={carouselItems.length}>
+                        <Row className="row justify-content-between">{currentRow}</Row>
+                    </Carousel.Item>
+                );
+                currentRow = [];
+            }
+        });
 
-							<div className="card-title">{vehicle.name}</div>
+        return carouselItems;
+    }
 
-							<button className="favorite-button" onClick={() => addToFavorites(vehicle)}>
-								Añadir a favoritos
-							</button>
-						</div>
-					))}
-				</div>
-			</section>
+    function openDetails(item) {
+        setSelectedFavorite(item);
+    }
 
+    return (
+        <div className="container">
+            <header className="header">
+                <h1>Star Wars</h1>
+            </header>
+            <Navbar className="favorites-navbar" bg="light" expand="lg">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link onClick={() => setShowFavoritesModal(true)}>
+                                Mis Favoritos ({favorites.length})
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className="content">
+                <section className="section">
+                    <h2>Planetas</h2>
+                    <Carousel>{createCarouselItems(store.planets, itemsPerRow)}</Carousel>
+                </section>
+                <section className="section">
+                    <h2>Personajes</h2>
+                    <Carousel>{createCarouselItems(store.characters, itemsPerRow)}</Carousel>
+                </section>
+                <section className="section">
+                    <h2>Vehículos</h2>
+                    <Carousel>{createCarouselItems(store.vehicles, itemsPerRow)}</Carousel>
+                </section>
+            </div>
 
-			{selectedItem && <DetailPage selectedItem={selectedItem} onClose={handleCloseDetail} />}
-		</div>
-	);
+            <Modal show={showFavoritesModal} onHide={() => setShowFavoritesModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Mis Favoritos</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {favorites.length > 0 ? (
+                        <ul className="list-group">
+                            {favorites.map((favItem) => (
+                                <li key={favItem.id} className="list-group-item d-flex justify-content-between align-items-center">
+                                    {favItem.name}
+                                    <button
+                                        className="btn btn-danger"
+                                        onClick={() => removeFromFavorites(favItem)}
+                                    >
+                                        Eliminar
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>No tienes favoritos aún.</p>
+                    )}
+                </Modal.Body>
+            </Modal>
+            <Modal show={selectedFavorite} onHide={() => setSelectedFavorite(null)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Detalles</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Card>
+                        <Card.Img
+                            variant="top"
+                            src={selectedFavorite ? selectedFavorite.img : ""}
+                            alt={selectedFavorite ? selectedFavorite.name : ""}
+                        />
+                        <Card.Body>
+                            <Card.Title>
+                                {selectedFavorite ? selectedFavorite.name : ""}
+                            </Card.Title>
+                           
+                        </Card.Body>
+                    </Card>
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
 };
